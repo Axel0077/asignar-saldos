505fa0f947b8b96d65fabf05b3e9321bb7766824
Axel0077 <axelleonardoperezm@gmail.com>
2024-12-07 16:43:51 -0600

    creando controller para exponer el servicio

---
+++ asignar-saldos/Controllers/SaldoController.cs
@@ -0,0 +1,23 @@
+﻿using asignar_saldos.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SaldoController : ControllerBase
+{
+    private readonly SaldoService _saldoService;
+
+    public SaldoController(SaldoService saldoService)
+    {
+        _saldoService = saldoService;
+    }
+
+    // Endpoint para obtener los saldos asignados a los gestores
+    [HttpGet("asignar-saldos")]
+    public IActionResult AsignarSaldosAGestores()
+    {
+        var saldosAsignados = _saldoService.AsignarSaldosAGestores();
+        return Ok(saldosAsignados);
+    }
+}
+
